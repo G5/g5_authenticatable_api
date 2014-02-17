b2faec1c6e9adeaa1aee99ee59f3f05e15268652
maeve <maeve.revels@getg5.com>
2014-02-17 12:13:32 +0000

    Minor refactoring of auth header logic

--- lib/g5_authenticatable_api/grape_helpers.rb
+++ lib/g5_authenticatable_api/grape_helpers.rb
@@ -5,13 +5,12 @@ module G5AuthenticatableApi
         begin
           g5_auth_client.token_info
         rescue OAuth2::Error => error
-          error_code = error.code || 'invalid_request'
-          auth_header = "Bearer error=\"#{error_code}\""
-          auth_header << ",error_description=\"#{error.description}\"" if error.description
-          throw :error, status: 401, headers: {'WWW-Authenticate' => auth_header}
+          throw :error, status: 401,
+                        headers: {'WWW-Authenticate' => authenticate_header(error)}
         end
       else
-        throw :error, status: 401, headers: {'WWW-Authenticate' => 'Bearer'}
+        throw :error, status: 401,
+                      headers: {'WWW-Authenticate' => authenticate_header}
       end
     end
 
@@ -26,6 +25,19 @@ module G5AuthenticatableApi
 
     def g5_auth_client
       G5AuthenticationClient::Client.new(access_token: access_token)
+    end
+
+    private
+    def authenticate_header(error=nil)
+      auth_header = "Bearer"
+
+      if error
+        error_code = error.code || 'invalid_request'
+        auth_header << " error=\"#{error_code}\""
+        auth_header << ",error_description=\"#{error.description}\"" if error.description
+      end
+
+      auth_header
     end
   end
 end
