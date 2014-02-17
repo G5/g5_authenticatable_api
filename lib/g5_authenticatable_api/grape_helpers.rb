5f1e067e8c88eaa6dd36880da45c1de0f4a8e489
maeve <maeve.revels@getg5.com>
2014-02-17 11:54:24 +0000

    Implement check for presence of access token

--- lib/g5_authenticatable_api/grape_helpers.rb
+++ lib/g5_authenticatable_api/grape_helpers.rb
@@ -1,7 +1,22 @@
 module G5AuthenticatableApi
   module GrapeHelpers
     def authenticate_user!
-      throw :error, status: 401, headers: {'WWW-Authenticate' => 'Bearer'}
+      unless access_token
+        throw :error, status: 401, headers: {'WWW-Authenticate' => 'Bearer'}
+      end
+    end
+
+    def access_token
+      @access_token ||= if env['HTTP_AUTHORIZATION']
+        parts = env['HTTP_AUTHORIZATION'].match(/Bearer (?<access_token>\S+)/)
+        parts['access_token']
+      else
+        Rack::Request.new(env).params['access_token']
+      end
+    end
+
+    def g5_auth_client
+      G5AuthenticationClient::Client.new(access_token: access_token)
     end
   end
 end
