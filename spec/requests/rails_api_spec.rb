1a628f0072a752c1ab69a95500c0e2b5bc2ed1eb
maeve <maeve.revels@getg5.com>
2014-02-23 19:50:30 +0000

    Initial request specs around rails api

---
+++ spec/requests/rails_api_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'A Secure Rails API endpoint' do
+  let(:params) {}
+  let(:headers) {}
+
+  describe 'GET articles index' do
+    subject(:api_call) { get '/rails_api/articles', params, headers }
+
+    it_should_behave_like 'a warden authenticatable api'
+    it_should_behave_like 'a token authenticatable api'
+  end
+end
