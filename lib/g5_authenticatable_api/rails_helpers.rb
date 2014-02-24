763e65c839b1c71b7b09708441af6522c6dc324b
maeve <maeve.revels@getg5.com>
2014-02-24 02:06:17 +0000

    Check warden user in rails before_filter

--- lib/g5_authenticatable_api/rails_helpers.rb
+++ lib/g5_authenticatable_api/rails_helpers.rb
@@ -1,7 +1,14 @@
 module G5AuthenticatableApi
   module RailsHelpers
     def authenticate_api_user!
-      raise 'implement authenticate_api_user!'
+      unless warden.try(:authenticated?)
+        render status: :unauthorized,
+               text: {error: 'Unauthorized'}.to_json
+      end
+    end
+
+    def warden
+      request.env['warden']
     end
   end
 end
