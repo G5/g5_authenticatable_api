28f76dec4061353112ef809622b184b92722f883
maeve <maeve.revels@getg5.com>
2014-02-25 03:26:49 +0000

    Refactor TokenValidator specs for clarity

--- spec/lib/g5_authenticatable_api/token_validator_spec.rb
+++ spec/lib/g5_authenticatable_api/token_validator_spec.rb
@@ -6,26 +6,75 @@ describe G5AuthenticatableApi::TokenValidator do
   let(:validator) { described_class.new(params, headers) }
 
   let(:headers) {}
-  let(:params) {}
+  let(:params) { {'access_token' => token_value} }
+  let(:token_value) { 'abc123' }
 
-  let(:access_token) { 'abc123' }
+  describe '#access_token' do
+    subject(:access_token) { validator.access_token }
 
-  context 'with Authorization header' do
-    let(:headers) { {'Authorization' => "Bearer #{access_token}"} }
+    context 'with auth header' do
+      let(:headers) { {'Authorization' => "Bearer #{token_value}"} }
+      let(:params) {}
+
+      it 'should extract the token value from the header' do
+        expect(access_token).to eq(token_value)
+      end
+    end
+
+    context 'with auth param' do
+      let(:params) { {'access_token' => token_value} }
+      let(:headers) {}
+
+      it 'should extract the token value from the access_token parameter' do
+        expect(access_token).to eq(token_value)
+      end
+    end
+  end
+
+  describe '#validate_token!' do
+    subject(:validate_token!) { validator.validate_token! }
 
     context 'when token is valid' do
       include_context 'valid access token'
 
-      its(:access_token) { should == access_token }
-
       it 'should initialize the auth client with the access token' do
         validator.auth_client
-        expect(G5AuthenticationClient::Client).to have_received(:new).with(access_token: access_token)
+        expect(G5AuthenticationClient::Client).to have_received(:new).with(access_token: token_value)
       end
 
       it 'should not raise errors during validation' do
-        expect { validator.validate_token! }.to_not raise_error
+        expect { validate_token! }.to_not raise_error
       end
+    end
+
+    context 'when token is invalid' do
+      include_context 'invalid access token'
+
+      it 'should initialize the auth client with the access token' do
+        validator.auth_client
+        expect(G5AuthenticationClient::Client).to have_received(:new).with(access_token: token_value)
+      end
+
+      it 'should re-raise the OAuth error' do
+        expect { validate_token! }.to raise_error(OAuth2::Error)
+      end
+    end
+
+    context 'when there is no token' do
+      let(:params) {}
+      let(:headers) {}
+
+      it 'should raise an error' do
+        expect { validate_token! }.to raise_error
+      end
+    end
+  end
+
+  describe '#valid?' do
+    subject(:valid?) { validator.valid? }
+
+    context 'when token is valid' do
+      include_context 'valid access token'
 
       it 'should be valid' do
         expect(validator).to be_valid
@@ -35,44 +84,18 @@ describe G5AuthenticatableApi::TokenValidator do
     context 'when token is invalid' do
       include_context 'invalid access token'
 
-      it 'should re-raise the OAuth error' do
-        expect { validator.validate_token! }.to raise_error(OAuth2::Error)
-      end
-
-      it 'should be invalid' do
+      it 'should not be valid' do
         expect(validator).to_not be_valid
       end
     end
-  end
 
-  context 'with access_token parameter' do
-    let(:params) { {'access_token' => access_token} }
+    context 'without token' do
+      let(:params) {}
+      let(:headers) {}
 
-    include_context 'valid access token'
-
-    its(:access_token) { should == access_token }
-
-    it 'should initialize the auth client with the access token' do
-      validator.auth_client
-      expect(G5AuthenticationClient::Client).to have_received(:new).with(access_token: access_token)
-    end
-
-    it 'should not raise errors during validation' do
-      expect { validator.validate_token! }.to_not raise_error
-    end
-
-    it 'should be valid' do
-      expect(validator).to be_valid
-    end
-  end
-
-  context 'without token' do
-    it 'should raise an error during validation' do
-      expect { validator.validate_token! }.to raise_error
-    end
-
-    it 'should not be valid' do
-      expect(validator).to_not be_valid
+      it 'should not be valid' do
+        expect(validator).to_not be_valid
+      end
     end
   end
 end
