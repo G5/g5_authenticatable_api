f5b40e9941069c254f3bb4bb5c800e07294e87e6
maeve <maeve.revels@getg5.com>
2014-03-07 20:46:36 +0000

    Test case that illustrates bug with password credentials

--- spec/support/shared_examples/token_authenticatable_api.rb
+++ spec/support/shared_examples/token_authenticatable_api.rb
@@ -81,4 +81,26 @@ shared_examples_for 'a token authenticatable api' do
       expect(response.headers).to include('WWW-Authenticate' => 'Bearer')
     end
   end
+
+  context 'with environment variables for password credentials' do
+    before do
+      ENV['G5_AUTH_USERNAME'] = 'my.user@test.host'
+      ENV['G5_AUTH_PASSWORD'] = 'my_secret'
+    end
+
+    after do
+      ENV['G5_AUTH_USERNAME'] = nil
+      ENV['G5_AUTH_PASSWORD'] = nil
+    end
+
+    before { subject }
+
+    it 'should be unauthorized' do
+      expect(response).to be_http_unauthorized
+    end
+
+    it 'should return an authenticate header without details' do
+      expect(response.headers).to include('WWW-Authenticate' => 'Bearer')
+    end
+  end
 end
