75bdd71e916951d62e6232cf4358d3b043c9030b
maeve <maeve.revels@getg5.com>
2015-01-12 23:13:03 +0000

    Add config option for strict_token_validation'

--- lib/g5_authenticatable_api.rb
+++ lib/g5_authenticatable_api.rb
@@ -1,3 +1,6 @@
+require 'active_support'
+require 'active_support/core_ext/module/attribute_accessors'
+
 require 'g5_authenticatable_api/version'
 require 'g5_authenticatable_api/helpers/grape'
 require 'g5_authenticatable_api/railtie' if defined?(Rails)
@@ -5,5 +8,9 @@ require 'g5_authenticatable_api/railtie' if defined?(Rails)
 require 'g5_authentication_client'
 
 module G5AuthenticatableApi
-  # Your code goes here...
+  # When enabled, strict token validation will validate the session user's
+  # access_token against the auth server for every request (if there is
+  # an existing session in warden). Disabled by default.
+  @@strict_token_validation = false
+  mattr_accessor :strict_token_validation
 end
