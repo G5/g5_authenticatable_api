80d84e6a232f39df1ff1adf24565de6d035e463f
maeve <maeve.revels@getg5.com>
2015-05-19 23:07:19 +0000

    Unit test coverage for the helpers

---
+++ spec/lib/g5_authenticatable_api/helpers/grape_spec.rb
@@ -0,0 +1,82 @@
+require 'spec_helper'
+
+describe G5AuthenticatableApi::Helpers::Grape do
+  include Rack::Test::Methods
+
+  def app
+    Class.new(Grape::API) do
+      helpers G5AuthenticatableApi::Helpers::Grape
+
+      before { authenticate_user! }
+
+      get :index do
+        { hello: 'world' }
+      end
+    end
+  end
+
+  describe '#authenticate_user!' do
+    subject(:authenticate_user!) { get :index, params, env }
+    let(:params) { {'access_token' => token_value} }
+    let(:env) { {'warden' => warden} }
+    let(:warden) { double(:warden) }
+
+    let(:token_value) { 'abc123' }
+
+    let(:token_validator) do
+      double(:token_validator, valid?: valid,
+                               auth_response_header: auth_response_header)
+    end
+    before do
+      allow(G5AuthenticatableApi::TokenValidator).to receive(:new).
+        and_return(token_validator)
+    end
+
+    context 'when token is valid' do
+      let(:valid) { true }
+      let(:auth_response_header) {}
+
+      it 'initializes the token validator correctly' do
+        authenticate_user!
+        expect(G5AuthenticatableApi::TokenValidator).to have_received(:new).
+          with(params,
+               {'Host'=>'example.org', 'Cookie'=>''},
+               warden)
+      end
+
+      it 'is successful' do
+        authenticate_user!
+        expect(last_response).to be_http_ok
+      end
+
+      it 'does not set the authenticate response header' do
+        authenticate_user!
+        expect(last_response).to_not have_header('WWW-Authenticate')
+      end
+    end
+
+    context 'when token is invalid' do
+      let(:valid) { false }
+      let(:auth_response_header) { 'whatever' }
+
+      it 'is unauthorized' do
+        authenticate_user!
+        expect(last_response).to be_http_unauthorized
+      end
+
+      it 'renders an error message' do
+        authenticate_user!
+        expect(last_response.body).to eq('Unauthorized')
+      end
+
+      it 'sets the authenticate response header' do
+        authenticate_user!
+        expect(last_response).to have_header('WWW-Authenticate' => auth_response_header)
+      end
+    end
+  end
+
+  describe '#current_auth_user' do
+    pending 'implement me!'
+  end
+end
---
+++ spec/lib/g5_authenticatable_api/helpers/rails_spec.rb
@@ -0,0 +1,74 @@
+require 'spec_helper'
+
+describe G5AuthenticatableApi::Helpers::Rails, type: :controller do
+  controller(ActionController::Base) do
+    before_action :authenticate_api_user!
+
+    def index
+      render json: [], status: :ok
+    end
+  end
+
+  describe '#authenticate_api_user!' do
+    subject(:authenticate_api_user!) { get :index, access_token: token_value }
+
+    let(:token_value) { 'abc123' }
+
+    let(:token_validator) do
+      double(:token_validator, valid?: valid,
+                               auth_response_header: auth_response_header)
+    end
+    before do
+      allow(G5AuthenticatableApi::TokenValidator).to receive(:new).
+        and_return(token_validator)
+    end
+
+    context 'when token is valid' do
+      let(:valid) { true }
+      let(:auth_response_header) {}
+
+      it 'initializes the token validator correctly' do
+        authenticate_api_user!
+        expect(G5AuthenticatableApi::TokenValidator).to have_received(:new).
+          with(request.params,
+               an_instance_of(ActionDispatch::Http::Headers),
+               request.env['warden'])
+      end
+
+      it 'is successful' do
+        authenticate_api_user!
+        expect(response).to be_success
+      end
+
+      it 'does not set the authenticate response header' do
+        authenticate_api_user!
+        expect(response).to_not have_header('WWW-Authenticate')
+      end
+    end
+
+    context 'when token is invalid' do
+      let(:valid) { false }
+      let(:auth_response_header) { 'whatever' }
+
+      it 'is unauthorized' do
+        authenticate_api_user!
+        expect(response).to be_unauthorized
+      end
+
+      it 'renders an error message' do
+        authenticate_api_user!
+        expect(JSON.parse(response.body)).to eq('error' => 'Unauthorized')
+      end
+
+      it 'sets the authenticate response header' do
+        authenticate_api_user!
+        expect(response).to have_header('WWW-Authenticate' => auth_response_header)
+      end
+    end
+  end
+
+  describe '#current_auth_user' do
+    pending 'implement me!'
+  end
+end
+
